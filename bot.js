310191ac89292c58feb485929d162d0a20721d85
RedBot <46789490+GreeGhost123@users.noreply.github.com>
2019-09-11 23:35:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -284,6 +284,11 @@ client.on('message', async message => {
     }
 });
 });
+const Eris = require("eris");
+client.on("ready", () => {
+    console.log("Yo! Changer [ON]");
+    onstart();
+});
 x5bz = "621423685567381540";
 nick = "Draw-Bot☥";
 function onstart(){
@@ -306,7 +311,31 @@ var x5bz2 = ' ';
    
      }, 1000);
 }
-x5bzteam.connect();
+client.connect();
+
+x5bz = "621423685567381540";
+nick = "Draw-Bot☥";
+function onstart(){
+    var v1 = nick.split('');
+    var counter;
+    var i=0;
+var x5bz2 = ' ';
+  var x5bz3 =   setInterval(function(){
+     
+     if (i == v1.length){
+     clearInterval(x5bz3);
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+    x5bz2 += v2;
+    x5bzteam.editNickname(x5bz, x5bz2);
+ 
+        i++;
+   
+     }, 1000);
+}
+client.connect();
 
 client.on('message', message => {
      if(!message.channel.guild) return;
